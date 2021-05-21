ea9833c0231c32c747e613c150da56bdc6b0a484
pmqueiroz <pedroqueiroz593@gmail.com>
2021-05-20 22:26:44 -0300

    test: update test

--- index.spec.js
+++ index.spec.js
@@ -1,6 +1,8 @@
-
 const { time } = require('.')
 
 test('mask time input', () => {
-   expect(time(1312)).toBe('13h12m');
-});
+   expect(time(1)).toBe('0h01m')
+   expect(time(13)).toBe('0h13m')
+   expect(time(131)).toBe('1h31m')
+   expect(time(1312)).toBe('13h12m')
+})
